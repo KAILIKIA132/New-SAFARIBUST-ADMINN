de9f387f148edfae66559c82ff53473ede7ff4ca
aaron <amkailikia@gmail.com>
2023-08-29 11:21:35 +0300

    updated

--- src/utils/faker.ts
+++ src/utils/faker.ts
@@ -11,7 +11,8 @@ interface Products {
   name: string;
   category: string;
 }
-//devops samson safari
+//var/lib/jenkins/.nvm/versions/node/v16.17.0/bin/node
+//devops samson safari has dio it agaion
 interface Categories {
   name: string;
   tags: string;

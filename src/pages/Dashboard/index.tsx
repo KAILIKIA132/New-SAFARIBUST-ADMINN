42cadb5c7917d71652676b0b76c9b26f4f71c48b
aaron <amkailikia@gmail.com>
2023-08-29 15:03:40 +0300

    updated

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -92,7 +92,7 @@ function Main() {
                   <div className="ml-auto">
                     <Tippy
                       as="div"
-                      className="cursor-pointer bg-info py-[3px] flex rounded-full text-white text-xs pl-2 pr-1 items-center font-medium"
+                      className="cursor-pointer bg-blue-500 py-[3px] flex rounded-full text-white text-xs pl-2 pr-1 items-center font-medium"
                       content=""
                     >
                       <Lucide icon="ChevronRight" className="w-4 h-4 ml-0.5" />
@@ -128,7 +128,7 @@ function Main() {
                   <div className="ml-auto">
                     <Tippy
                       as="div"
-                      className="cursor-pointer bg-info py-[3px] flex rounded-full text-white text-xs pl-2 pr-1 items-center font-medium"
+                      className="cursor-pointer bg-blue-500 py-[3px] flex rounded-full text-white text-xs pl-2 pr-1 items-center font-medium"
                       content=""
                     >
                       <Lucide icon="ChevronRight" className="w-4 h-4 ml-0.5" />
@@ -159,10 +159,10 @@ function Main() {
                     icon="Volume2"
                     className="w-[28px] h-[28px] text-blue-500"
                   />
-                  <div className="ml-auto">
+                  <div className="ml-auto ">
                     <Tippy
                       as="div"
-                      className="cursor-pointer bg-info py-[3px] flex rounded-full text-white text-xs pl-2 pr-1 items-center font-medium"
+                      className="cursor-pointer bg-blue-500 py-[3px] flex rounded-full text-white text-xs pl-2 pr-1 items-center font-medium"
                       content=""
                     >
                       <Lucide icon="ChevronRight" className="w-4 h-4 ml-0.5" />
@@ -195,7 +195,7 @@ function Main() {
                   <div className="ml-auto">
                     <Tippy
                       as="div"
-                      className="cursor-pointer bg-info py-[3px] flex rounded-full text-white text-xs pl-2 pr-1 items-center font-medium"
+                      className="cursor-pointer bg-blue-500 py-[3px] flex rounded-full text-white text-xs pl-2 pr-1 items-center font-medium"
                       content=""
                     >
                       <Lucide icon="ChevronRight" className="w-4 h-4 ml-0.5" />
@@ -229,7 +229,7 @@ function Main() {
                   <div className="ml-auto">
                     <Tippy
                       as="div"
-                      className="cursor-pointer bg-info py-[3px] flex rounded-full text-white text-xs pl-2 pr-1 items-center font-medium"
+                      className="cursor-pointer bg-blue-500 py-[3px] flex rounded-full text-white text-xs pl-2 pr-1 items-center font-medium"
                       content=""
                     >
                       <Lucide icon="ChevronRight" className="w-4 h-4 ml-0.5" />

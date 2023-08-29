cdf17778b6d7344b99c8b41f6e2185a5c6d0a00c
aaron <amkailikia@gmail.com>
2023-08-29 14:15:53 +0300

    updated

--- src/pages/model/index.tsx
+++ src/pages/model/index.tsx
@@ -229,7 +229,7 @@ function Main() {
                       {user.approval_status ? "Active" : "Inactive"} */}
                     </div>
                   </Table.Td>
-                  <Table.Td className="first:rounded-l-md last:rounded-r-md bg-white border-b-0 dark:bg-darkmode-600 shadow-[20px_3px_20px_#0000000b] py-0 relative before:block before:w-px before:h-8 before:bg-slate-200 before:absolute before:left-0 before:inset-y-0 before:my-auto before:dark:bg-darkmode-400">
+                  {/* <Table.Td className="first:rounded-l-md last:rounded-r-md bg-white border-b-0 dark:bg-darkmode-600 shadow-[20px_3px_20px_#0000000b] py-0 relative before:block before:w-px before:h-8 before:bg-slate-200 before:absolute before:left-0 before:inset-y-0 before:my-auto before:dark:bg-darkmode-400">
                     <div className="flex items-center justify-center">
                       <Menu>
                         <Menu.Button as={Button} className="px-2 !box">
@@ -265,7 +265,7 @@ function Main() {
                         </Menu.Items>
                       </Menu>
                     </div>
-                  </Table.Td>
+                  </Table.Td> */}
                 </Table.Tr>
               ))}
             </Table.Tbody>
